e397b3798d6abfc7569a6d49da59a913fc2d7df5
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2024-12-31 14:42:53 -0300

    description

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className={`${poppins.className}`}>
       <head>
         <title>Enlightish</title>
-        <meta name="description" content="Descrição do meu projeto incrível" />
+        <meta name="description" content="Escola de idiomas com foco no inglês cristão." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </head>
