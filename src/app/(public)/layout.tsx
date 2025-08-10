136455f8f8f4d20b1dabe521ace32f4b56432a8d
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2025-08-10 01:48:28 -0300

    style: adjust z-index whatsapp button

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,37 +1,51 @@
-import { Topbar } from "@/components/Topbar";
+import { Topbar } from '@/components/Topbar';
 import { Poppins } from 'next/font/google';
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
-config.autoAddCss = false
+config.autoAddCss = false;
 
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'],
+    display: 'swap'
 });
 
+export default function RootLayout({
+    children
+}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en" className={`${poppins.className}`}>
+            <head>
+                <title>Enlightish</title>
+                <meta
+                    name="description"
+                    content="Escola de idiomas com foco no inglês cristão."
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/assets/favicon-logo.png" />
+            </head>
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+            <body className="overflow-x-hidden">
+                <Topbar />
+                {children}
 
-  return (
-    <html lang="en" className={`${poppins.className}`}>
-      <head>
-        <title>Enlightish</title>
-        <meta name="description" content="Escola de idiomas com foco no inglês cristão." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/assets/favicon-logo.png" />
-      </head>
-
-      <body className="overflow-x-hidden">
-        <Topbar />
-        {children}
-
-        <a href="https://wa.me/5521982794750" target="_blank" rel="noopener noreferrer" className="fixed right-6 bottom-6 lg:right-8 lg:bottom-8">
-            <img src="/assets/whatsapp.svg" className="h-14 w-14" alt="WhatsApp Link" />
-        </a>
-      </body>
-    </html>
-  );
-
+                <a
+                    href="https://wa.me/5521982794750"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="fixed right-6 bottom-6 lg:right-8 lg:bottom-8 z-50"
+                >
+                    <img
+                        src="/assets/whatsapp.svg"
+                        className="h-14 w-14"
+                        alt="WhatsApp Link"
+                    />
+                </a>
+            </body>
+        </html>
+    );
 }
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { Footer } from '@/components/Footer';
 import '../../global/globals.css';
@@ -12,22 +12,22 @@ import { useEffect } from 'react';
 
 export default function Home() {
     useEffect(() => {
-        document.documentElement.style.scrollBehavior = "unset";
+        document.documentElement.style.scrollBehavior = 'unset';
 
         setTimeout(() => {
-            document.documentElement.style.scrollBehavior = "smooth";
-        }, 0); 
-   }, []);
+            document.documentElement.style.scrollBehavior = 'smooth';
+        }, 0);
+    }, []);
 
-  return (
-    <>
-      <Start />
-      <Intro />
-      <About />
-      <Services />
-      <Feedback />
-      <Contact />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Start />
+            <Intro />
+            <About />
+            <Services />
+            <Feedback />
+            <Contact />
+            <Footer />
+        </>
+    );
 }
