0cc8f1fb41a60f9108ac6675a261c8c6b21c20b7
Leticia Lopes <107498937+LeticiaLopess@users.noreply.github.com>
2025-08-24 15:07:07 -0300

    style: refine checkbox

--- src/components/Check/index.tsx
+++ src/components/Check/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function Check() {
   return (
-        <div className='flex items-center justify-center p-1.5 bg-crayola-50 border border-crayola rounded-lg w-5 h-5'>
+        <div className='flex items-center justify-center p-[5px] bg-crayola-50 border border-crayola rounded-lg w-5 h-5'>
             <Image
                 src="/assets/icon-check.svg"
                 alt="Check"
